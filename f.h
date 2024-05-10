6c95cb5b2b9f53af5e0cce7902539e698f2307b0
Jsckp <168212490+Jsckp@users.noreply.github.com>
2024-05-10 13:35:31 -0500

    Add files via upload

---
+++ f.h
@@ -0,0 +1,5 @@
+
+float calculoInteres(float saldo, float tasaInteres);
+float calcularTotalInteres(float monto, float interes, int tiempo);
+float calcularTotalMonto(float monto, int tiempo);
+void mostrarTabla(float monto, float interes, int tiempo);
